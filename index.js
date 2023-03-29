3d3721ecff83a160213ffe130f0159e843e533af
Addo Michael <michaeladdo1997@gmail.com>
2023-03-29 23:13:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ searchBtn.addEventListener('click',()=>{
     let url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
     // console.log(url)
     fetch(url).then((res)=>res.json()).then((data)=>{
-        console.log(data[0])
-        console.log(data[0].capital[0])
-        console.log(data[0].flags.svg)
-        console.log(data[0].name.common)
-        console.log(data[0].continents[0])
-        console.log(Object.keys(data[0].currencies)[0])
-        console.log(data[0].currencies[Object.keys(data[0].currencies)].name)
-        console.log(Object.values(data[0].languages).toString().split(',').join(','));
+        // console.log(data[0])
+        // console.log(data[0].capital[0])
+        // console.log(data[0].flags.svg)
+        // console.log(data[0].name.common)
+        // console.log(data[0].continents[0])
+        // console.log(Object.keys(data[0].currencies)[0])
+        // console.log(data[0].currencies[Object.keys(data[0].currencies)].name)
+        // console.log(Object.values(data[0].languages).toString().split(',').join(','));
 
 
         results.innerHTML = `
